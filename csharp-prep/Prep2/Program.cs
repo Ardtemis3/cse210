6e5c867bb21793fb4aab7d92ca718fa22ba89036
Ken Ard III <kennyard3@byui.edu>
2025-01-13 14:56:51 -0700

    part 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
-class Program
+class Grade
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percent? ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+        if (gradeNumber >= 90){
+            Console.Write("Your grade is an A. ");
+        }
+        else if (gradeNumber >= 80){
+            Console.Write("Your grade is an B. ");
+        }
+        else if (gradeNumber >= 70){
+            Console.Write("Your grade is an C. ");
+        }
+        else if (gradeNumber >= 60){
+            Console.Write("Your grade is an D. ");
+        }
+        else {
+            Console.Write("Your grade is an F. ");
+        }
+
     }
 }
