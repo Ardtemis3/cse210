84973b5481ef9a900ac53e139a82328f5e75c8f5
Ken Ard III <kennyard3@byui.edu>
2025-02-08 20:45:37 -0700

    update

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+        Console.WriteLine("Enter the numerator:");
+        string numerator = Console.ReadLine();
+        Console.WriteLine("Enter a denominator: ");
+        string denominator = Console.ReadLine();
     }
 }
