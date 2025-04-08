a99ef3f5b4df49a624e84d88977104053c9d2014
Ken Ard III <kennyard3@byui.edu>
2025-04-07 23:29:28 -0600

    Co-authored-by: Caleb Willden <carcabob@gmail.com>

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,9 +3,21 @@ using System;
 class Program
 {
     static void Main(){
-        Scripture scripture = new Scripture();
-        Verse verse = new Verse();
-        verse.Load();
-        verse.Display();
+
+        // Create string lists
+        List<string> _verseWords1 = new List<string> {"Trust", "in", "the", "Lord", "with", "all", "thine", "heart;", "and", "lean", "not", "unto", "thine", "own", "understanding."};
+        List<string> _verseWords2 = new List<string> {"In", "all", "thy", "ways", "acknowledge", "him,", "and", "he", "shall", "direct", "thy", "paths."};
+
+        // Create verses from string lists
+        Verse verse1 = new Verse(_verseWords1);
+        Verse verse2 = new Verse(_verseWords2);
+
+        // Create list of verses
+        List<Verse> _listOfVerses = new List<Verse> {verse1, verse2};
+
+        // Create scripture
+        Scripture scripture = new Scripture(_listOfVerses);
+        
     }
+
 }
--- prove/Develop03/Verse.cs
+++ prove/Develop03/Verse.cs
@@ -2,28 +2,8 @@ using System;
 
 class Verse
 {
-    
-
-        private Random random = new Random();
-        private List<Word> _listWord = new List<Word>{};
-
-//5: Trust in the Lord with all thine heart; and lean not unto thine own understanding.
-
-//6: In all thy ways acknowledge him, and he shall direct thy paths.
-
-        // HashSet<int> usedIndices = new HashSet<int>();
-
-        // Replace exactly 3 random words
-        // while (usedIndices.Count < 3)
-        // {
-        //     int randomIndex = random.Next(words.Count);
-        //     if (!usedIndices.Contains(randomIndex))
-        //     {
-        //         words[randomIndex] = new string('-', words[randomIndex].Length);
-        //         usedIndices.Add(randomIndex);
-        //     }
-        // }
-    
+    private Random random = new Random();
+    private List<Word> _listWord = new List<Word>{};
 
     public Verse(List<String> _fullVerse){
         foreach (string theWord in _fullVerse) {
